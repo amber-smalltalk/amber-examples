e3e3cc9637893f1dc0db54606c3345a0390f2833
Thomas Rake <tom.rake@gmail.com>
2013-04-18 08:47:25 -0400

    Added grunt to build nodejs examples

---
+++ nodejs/Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+  grunt.loadTasks('../vendor/amber/grunt/tasks');
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['amberc:hello']);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    amberc: {	
+		 _config: {
+			amber_dir: '../../vendor/amber',
+			closure_jar: ''
+		},
+		hello: {
+			working_dir: 'hello',
+			src: ['Hello.st'],
+			main_class: 'Hello',
+			output_name: 'Program'
+		 },
+		 benchfib: {
+			working_dir: 'benchfib',
+			src: ['Benchfib.st'],
+			main_class: 'Benchfib',
+			output_name: 'Program'
+		},
+		pystone: {
+			working_dir: 'pystone',
+			src: ['Pystone.st'],
+			main_class: 'Pystone',
+			output_name: 'Program'
+		},
+		trivialserver: {
+			working_dir: 'trivialserver',
+			src: ['TrivialServer.st'],
+			main_class: 'TrivialServer',
+			output_name: 'Program'
+		}
+
+
+      }
+    });
+
+}
