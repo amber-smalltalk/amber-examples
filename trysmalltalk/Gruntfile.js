70e70e686830bd4ff87013669d85d8460a88d48e
Manfred Kroehnert <mkroehnert@users.sourceforge.net>
2013-05-25 23:14:21 +0200

    trysmalltalk: add Gruntfile.js

---
+++ trysmalltalk/Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.loadTasks('../vendor/amber/grunt/tasks/');
+
+  grunt.registerTask('default', ['amberc:trysmalltalk']);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('../package.json'),
+
+    amberc: {  
+      _config: {
+        amber_dir: '../vendor/amber',
+        closure_jar: ''
+      },
+      trysmalltalk: {
+        src: ['st/TrySmalltalk.st'],
+        output_dir: 'js'
+      }
+    }
+  });
+
+}
