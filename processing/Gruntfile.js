31c83dd159e8a284878f531420dae1d67394e48f
Manfred Kroehnert <mkroehnert@users.sourceforge.net>
2013-05-25 23:14:01 +0200

    processing: add Gruntfile.js

---
+++ processing/Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.loadTasks('../vendor/amber/grunt/tasks/');
+
+  grunt.registerTask('default', ['amberc:processing']);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('../package.json'),
+
+    amberc: {  
+      _config: {
+        amber_dir: '../vendor/amber',
+        closure_jar: ''
+      },
+      processing: {
+        src: ['st/Processing-Examples.st'],
+        output_dir: 'js'
+      }
+    }
+  });
+
+}
