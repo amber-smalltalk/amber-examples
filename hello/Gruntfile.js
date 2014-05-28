a1a6783fc3357430ddb00b68e7b4753cc34b8bf3
Herbert Vojčík <herby@mailbox.sk>
2014-05-29 00:33:39 +0200

    Making hello build. It does not load, mysteriously.

---
+++ hello/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('../../node_modules/amber-dev');
+
+  grunt.registerTask('default', ['amberc:hello']);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('../package.json'),
+
+    amberc: {  
+      options: {
+        amber_dir: '../bower_components/amber',
+        closure_jar: ''
+      },
+      hello: {
+        src: ['st/HelloApp.st'],
+        libraries: ['Web'],
+        output_dir: 'js',
+        deploy: true,
+        amd_namespace: 'amber_examples_hello'
+      }
+    }
+  });
+
+}
--- hello/js/HelloApp.js
+++ hello/js/HelloApp.js
@@ -1,29 +1,34 @@
-define("com_example_hello/HelloApp", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_core/Kernel-Objects"], function(smalltalk,nil,_st){
+define("amber_examples_hello/HelloApp", ["amber/boot", "amber_core/Kernel-Objects"], function($boot){
+var smalltalk=$boot.vm,nil=$boot.nil,_st=$boot.asReceiver,globals=$boot.globals;
 smalltalk.addPackage('HelloApp');
-smalltalk.packages["HelloApp"].transport = {"type":"amd","amdNamespace":"com_example_hello"};
+smalltalk.packages["HelloApp"].transport = {"type":"amd","amdNamespace":"amber_examples_hello"};
 
-smalltalk.addClass('Hello', smalltalk.Object, [], 'HelloApp');
+smalltalk.addClass('Hello', globals.Object, [], 'HelloApp');
 smalltalk.addMethod(
 smalltalk.method({
 selector: "begin",
-category: 'not yet classified',
+protocol: 'not yet classified',
 fn: function (){
 var self=this;
 var msg,button;
 return smalltalk.withContext(function($ctx1) { 
+var $1,$2;
 msg="Hello world!";
 button="#sayHello"._asJQuery();
 _st(button)._click_((function(){
 return smalltalk.withContext(function($ctx2) {
-return _st(button)._after_(_st("<p>".__comma(msg)).__comma("</p>"));
+$1=button;
+$2=_st("<p>".__comma(msg)).__comma("</p>");
+$ctx2.sendIdx[","]=1;
+return _st($1)._after_($2);
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1,1)})}));
-return self}, function($ctx1) {$ctx1.fill(self,"begin",{msg:msg,button:button},smalltalk.Hello)})},
+return self}, function($ctx1) {$ctx1.fill(self,"begin",{msg:msg,button:button},globals.Hello)})},
 args: [],
 source: "begin\x0a\x22Makes me say hello to the user.\x22\x0a\x0a| msg button |\x0amsg := 'Hello world!'.\x0abutton := '#sayHello' asJQuery.\x0abutton click: [button after: '<p>' , msg , '</p>'].",
 messageSends: ["asJQuery", "click:", "after:", ","],
 referencedClasses: []
 }),
-smalltalk.Hello);
+globals.Hello);
 
 
 });
