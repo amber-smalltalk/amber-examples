4d552da2e61b39b27dd9a78c85876508d829c962
hhzl <hannes.hirzel@gmail.com>
2014-12-08 18:16:54 +0000

    removed test package

--- processing/devel.js
+++ processing/devel.js
@@ -1,9 +1,10 @@
 define([
     'amber/devel',
-    './deploy',
+    './deploy'
     // --- packages used only during development begin here ---
-    'amber-processingclock/ProcessingClock-Tests'
+
     // --- packages used only during development end here ---
 ], function (amber) {
     return amber;
 });
+
