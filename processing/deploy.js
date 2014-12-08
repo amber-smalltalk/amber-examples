dc215c63817adc0dbc436401a4565b235eeda719
hhzl <hannes.hirzel@gmail.com>
2014-12-08 17:52:34 +0000

    added files from amber-init

---
+++ processing/deploy.js
@@ -0,0 +1,8 @@
+define([
+    'amber/deploy',
+    // --- packages to be deployed begin here ---
+    'amber-processingclock/ProcessingClock'
+    // --- packages to be deployed end here ---
+], function (amber) {
+    return amber;
+});
---
+++ processing/devel.js
@@ -0,0 +1,9 @@
+define([
+    'amber/devel',
+    './deploy',
+    // --- packages used only during development begin here ---
+    'amber-processingclock/ProcessingClock-Tests'
+    // --- packages used only during development end here ---
+], function (amber) {
+    return amber;
+});
